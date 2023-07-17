7eaf004325717c985d9cff41191973dded131877
Everton Wallace Machado de Oliveira <102435699+xEvertonWallace@users.noreply.github.com>
2023-07-17 12:18:57 -0300

    inicialização de variaveis
    inicialização de variaveis a partir da biblioteca Scanner

--- course/src/application/Program.java
+++ course/src/application/Program.java
@@ -1,10 +1,23 @@
 package application;
 
+import java.util.Locale;
+import java.util.Scanner;
+
 public class Program {
 
 	public static void main(String[] args) {
+		Locale.setDefault(Locale.US);
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter acount number: ");
+		int number = sc.nextInt();
+		System.out.println("Enter account holder: ");
+		sc.nextLine();
+		String holder = sc.nextLine();
 		
 		
+		
+		
+		sc.close();
 	}
 
 }
