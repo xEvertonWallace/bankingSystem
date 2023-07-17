d77bd6b6764196561fecdfb6a414d9e18fa957b5
Everton Wallace Machado de Oliveira <102435699+xEvertonWallace@users.noreply.github.com>
2023-07-17 12:34:23 -0300

    print/update
    chamada da classe, impressão do resultado em tela e update do saldo em conta

--- course/src/application/Program.java
+++ course/src/application/Program.java
@@ -28,6 +28,17 @@ public class Program {
 			account = new Account(number, holder);
 		}
 		
+		System.out.println();
+		System.out.println("Account data");
+		System.out.println(account);
+		
+		System.out.println();
+		System.out.print("Enter a deposit value: ");
+		double depositValue = sc.nextDouble();
+		account.deposit(depositValue);
+		System.out.println("Update account data");
+		System.out.println(account);
+		
 		sc.close();
 	}
 
