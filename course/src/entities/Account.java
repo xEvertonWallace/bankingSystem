fbedc935fcf48ef0ced9f04089a8e8bbef08c77b
Everton Wallace Machado de Oliveira <102435699+xEvertonWallace@users.noreply.github.com>
2023-07-17 12:03:34 -0300

    class Acount
    criação da classe do tipo encapsulada Account que recebe os parametros para criação da conta do usuário

---
+++ course/src/entities/Account.java
@@ -0,0 +1,46 @@
+package entities;
+
+public class Account {
+	private int number;
+	private String holder;
+	private double balance;
+	
+	public Account(int number, String holder) {
+		this.number = number;
+		this.holder = holder;
+	}
+
+	public Account(int number, String holder, double initialDeposit) {
+		this.number = number;
+		this.holder = holder;
+		deposit(initialDeposit);
+	}
+
+	public String getHolder() {
+		return holder;
+	}
+
+	public void setHolder(String holder) {
+		this.holder = holder;
+	}
+
+	public int getNumber() {
+		return number;
+	}
+
+	public double getBalance() {
+		return balance;
+	}
+	
+	public void deposit(double amount) {
+		balance += amount;
+	}
+	
+	public void withDraw(double amount) {
+		balance -= amount + 5.0;
+	}
+	
+	
+	
+	
+}
