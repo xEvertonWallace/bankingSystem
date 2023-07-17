ced6c4091d1e2a4b23f2d658c702c0346a220398
Everton Wallace Machado de Oliveira <102435699+xEvertonWallace@users.noreply.github.com>
2023-07-17 12:25:34 -0300

    condição deposito
    criação da condição de deposito e importação da classe Account criada anteriormente.

--- course/src/application/Program.java
+++ course/src/application/Program.java
@@ -3,19 +3,30 @@ package application;
 import java.util.Locale;
 import java.util.Scanner;
 
+import entities.Account;
+
 public class Program {
 
 	public static void main(String[] args) {
 		Locale.setDefault(Locale.US);
 		Scanner sc = new Scanner(System.in);
+		Account account;
+		
 		System.out.println("Enter acount number: ");
 		int number = sc.nextInt();
 		System.out.println("Enter account holder: ");
 		sc.nextLine();
 		String holder = sc.nextLine();
-		
-		
-		
+		System.out.println("Is there an initial deposit (y/n)? ");
+		char response = sc.next().charAt(0);
+		if (response == 'y') {
+			System.out.println("Enter initial deposit value: ");
+			double initialDeposit = sc.nextDouble();
+			account = new Account(number, holder, initialDeposit);	
+		}
+		else {
+			account = new Account(number, holder);
+		}
 		
 		sc.close();
 	}
